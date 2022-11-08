773812a58319100b9d41f58677ac642c2572dbcc
Abidijihed <abidij55@gmail.com>
2022-11-08 15:34:48 +0100

    save

---
+++ chalange.js
@@ -0,0 +1,39 @@
+/*
+Queue Stack
+Write a stack. Once you’re done, implement a queue using two stacks to create FIFO behavior. 
+The queue should not have any storage separate from its stacks.
+*/
+
+var Stack = function() {
+    var storage = [];
+  
+    this.push = function(element) {
+       /*............*/
+    };
+  
+    this.pop = ()=>  ( /*............*/)
+        
+    
+  
+    this.size = ()=> ( /*............*/);
+           
+  
+  };
+  
+  var Queue = function() {
+    var inbox = new Stack();
+    var outbox = new Stack();
+  
+    this.enqueue = function(element) {
+  
+           
+        
+    };
+  
+    this.dequeue = function() {
+    
+    };
+  
+    this.size = ()=> 
+  
+  };
